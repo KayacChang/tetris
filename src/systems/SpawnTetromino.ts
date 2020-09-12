c09b317abf86a9ab4b3151945cd7b9f4f043c22d
kayac <egg734631@gmail.com>
2020-09-12 16:27:38 +0800

    add random tetromino

--- src/systems/SpawnTetromino.ts
+++ src/systems/SpawnTetromino.ts
@@ -1,10 +1,16 @@
 import { State } from "./types";
 import { Tetromino } from "../models/tetromino";
 
+function randomPick<T>(list: T[]) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
 export default function SpawnTetrominoSystem() {
   return (delta: number, state: State) => {
     if (!state.current || state.current?.lock) {
-      const current = Tetromino("I");
+      const current = Tetromino(
+        randomPick(["I", "J", "L", "O", "S", "T", "Z"])
+      );
 
       state.tetrominos.push(current);
       state.current = current;
