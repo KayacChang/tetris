7ec2307181a7e7602f005dff3e0d76478517a39d
kayac <egg734631@gmail.com>
2020-09-14 01:12:33 +0800

    Updates

---
+++ precache-manifest.91bc47be2865383f52f2da83f608d3af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b30cd96e11ed6fc3fc8a31872a67a9",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "82788f16ad25622fa1c4",
+    "url": "/tetris/static/css/main.b04386ec.chunk.css"
+  },
+  {
+    "revision": "658c7045f88484e032bc",
+    "url": "/tetris/static/js/2.ff0d45e5.chunk.js"
+  },
+  {
+    "revision": "2a33171a655e02a5c09f59af9651f8b3",
+    "url": "/tetris/static/js/2.ff0d45e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f27dd0adaa190f3a249f",
+    "url": "/tetris/static/js/3.82c7f011.chunk.js"
+  },
+  {
+    "revision": "82788f16ad25622fa1c4",
+    "url": "/tetris/static/js/main.f6a847c3.chunk.js"
+  },
+  {
+    "revision": "0451bea38165a5d5de88",
+    "url": "/tetris/static/js/runtime-main.3fa65c4e.js"
+  }
+]);
