36ab570040cba86dc31bc58fe72688b0bbe3ac79
kayac <egg734631@gmail.com>
2020-09-14 00:54:35 +0800

    add wall kicking

--- src/systems/MoveTetromino.ts
+++ src/systems/MoveTetromino.ts
@@ -3,11 +3,20 @@ import { mergeWith, add, equals } from "ramda";
 import { State } from "./types";
 import { inRange } from "../utils";
 
+function inPlayField(playField: number[][], x: number) {
+  return inRange(0, playField[0].length - 1, x);
+}
+
 function collide(
   playField: number[][],
   { blocks, rotate, vector, position }: ITetromino
 ) {
-  let isCollide = false;
+  const hit = {
+    block: false,
+    bottom: false,
+    border: false,
+    nudge: 0,
+  };
 
   blocks[rotate].forEach((row, py) => {
     row.forEach((exist, px) => {
@@ -20,15 +29,31 @@ function collide(
 
       const hitBlock = playField?.[y]?.[x];
       const hitBottom = y > playField.length - 1;
-      const hitBorder = !inRange(0, playField[0].length - 1, x);
+      const hitBorder = !inPlayField(playField, x);
 
-      if (hitBlock || hitBottom || hitBorder) {
-        isCollide = true;
+      if (hitBlock) {
+        hit.block = true;
+      }
+
+      if (hitBottom) {
+        hit.bottom = true;
+      }
+
+      if (hitBorder) {
+        hit.border = true;
+
+        const x = position.x + px;
+        const shouldNudge = !inPlayField(playField, x);
+
+        if (shouldNudge) {
+          const half = playField[0].length / 2;
+          hit.nudge = position.x > half ? -1 : 1;
+        }
       }
     });
   });
 
-  return isCollide;
+  return hit;
 }
 
 export default function MoveTetrominoSystem() {
@@ -37,17 +62,30 @@ export default function MoveTetrominoSystem() {
       return state;
     }
 
-    if (collide(state.playfield, state.current)) {
-      Object.assign(state.current, {
-        lock: equals(state.current.vector, { x: 0, y: 1 }),
+    const { playfield, current } = state;
+    const hit = collide(playfield, current);
+
+    if (hit.border) {
+      const { position } = current;
+      Object.assign(current, {
+        position: { x: position.x + hit.nudge, y: position.y },
         vector: { x: 0, y: 0 },
       });
 
       return state;
     }
 
-    const { position, vector } = state.current;
-    Object.assign(state.current, {
+    if (hit.bottom || hit.block) {
+      Object.assign(current, {
+        lock: equals(current.vector, { x: 0, y: 1 }),
+        vector: { x: 0, y: 0 },
+      });
+
+      return state;
+    }
+
+    const { position, vector } = current;
+    Object.assign(current, {
       position: mergeWith(add, position, vector),
       vector: { x: 0, y: 0 },
     });
